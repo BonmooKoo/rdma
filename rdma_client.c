1b696c94e355c3d28e6e80dc8bd079bc67119b9c
BONMOO KOO <rnqhs@DESKTOP-U4I8DG4>
2023-05-05 21:48:45 +0900

    A

---
+++ rdma_client.c
@@ -0,0 +1,132 @@
+#include "rdma.h"
+
+int main(){
+    device_list=ibv_get_device_list(NULL);
+    if(!device_list){
+        printf("No IB device\n");
+        exit(1);
+    }
+    device=device_list[0];
+    if(!device){
+        printf("No IB device\n");
+        exit(1);
+    }
+    context=ibv_open_device_by_ibdev_name("mlx5_0");
+    if(!context){
+        printf("No Context device\n");
+        exit(1);
+    }
+    pd=ibv_alloc_pd(context);
+    if(!pd){
+        printf("No protection domain\n");
+        exit(1);
+    }
+    msg=(char*)malloc(MSG_SIZE);
+    mem_region=ibv_reg_mr(pd,msg,MSG_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE);
+
+    if(!mem_region){
+        printf("No mem region\n");
+        exit(1);
+    }
+//Complete Queue 생성
+    comp_que=ibv_create_cq(context,1,NULL,NULL,0);
+    if(!comp_que){
+        printf("No comp_que\n");
+        exit(1);
+    }
+
+    struct ibv_qp_init_attr qp_init_attr={};
+    memset(&qp_init_attr,0,sizeof(qp_init_attr));
+    qp_init_attr.send_cq=comp_que;
+    qp_init_attr.recv_cq=comp_que;
+    qp_init_attr.qp_type=IBV_QPT_RC; // queue pair type ; reliable connection
+    qp_init_attr.cap.max_send_wr=1; // Work Request
+    qp_init_attr.cap.max_recv_wr=10;
+    qp_init_attr.cap.max_send_sge=1; //Scatter gather entry
+    qp_init_attr.cap.max_recv_sge=1;
+
+
+//Queue pair 생성
+    qp=ibv_create_qp(pd,&qp_init_attr);
+    if(!qp){
+        printf("No queue pair\n");
+        exit(1);
+    }
+// QP State 변경 
+// INIT > RTR / RTS
+// 수신만하는 쪽은 RTR도 ok
+    struct ibv_qp_attr conn_attr={};
+    memset(&conn_attr,0,sizeof(conn_attr));
+    conn_attr.qp_state = IBV_QPS_INIT;
+    conn_attr.path_mtu=IBV_MTU_256;
+    conn_attr.pkey_index = 0;
+    conn_attr.port_num = 1;
+    conn_attr.qp_access_flags = IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE;
+    if (ibv_modify_qp(qp, &conn_attr, IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT | IBV_QP_ACCESS_FLAGS)) {
+        fprintf(stderr, "Failed to modify QP state to INIT: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }//conn_attr로 설정한 내용을 qp에 저장
+    printf("QP %d : RTS state\n", qp->qp_num);
+/////////////////////////////////////////////////
+
+//송신버퍼 / 수신 버퍼 생성하고 QP에 등록하기
+//두개의 Queue pair을 통신 가능한 상태로 설정하기
+
+    char* send_buf = (char *) malloc(BUF_SIZE);
+    char* recv_buf = (char *) malloc(BUF_SIZE);
+    memset(send_buf, 0, BUF_SIZE);
+    memset(recv_buf, 0, BUF_SIZE);
+
+    // 송신, 수신 버퍼 메모리 등록
+    struct ibv_mr* send_mr = ibv_reg_mr(pd, send_buf, BUF_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ);
+    struct ibv_mr* recv_mr = ibv_reg_mr(pd, recv_buf, BUF_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ);
+
+    uintptr_t remote_addr=(uintptr_t) recv_buf;
+    remote_key=recv_mr->rkey;
+
+    //버퍼에 데이터 쓰기
+    sprintf(send_buf,"Hello RDMA! From Konduck1\n");
+
+    struct ibv_send_wr send_wr,*fail_send_wr;
+    struct ibv_sge sge;
+    memset(&sge, 0, sizeof(sge));
+    memset(&send_wr,0,sizeof(send_wr));
+    // sge / send_wr 설정하기
+    sge.addr = (uintptr_t)send_buf; //sge의 주소 설정하기
+    sge.length = BUF_SIZE;
+    sge.lkey = mr->lkey; //memory region의 lkey 설정하기
+    send_wr.wr_id = (uint64_t)(uintptr_t)sge.addr;
+    send_wr.opcode = IBV_WR_SEND_WITH_IMM;
+    send_wr.sg_list = &sge;
+    send_wr.num_sge = 1;
+    send_wr.imm_data = htonl(123456789); //임의의 32비트 값
+    ibv_post_send(qp,&send_wr,&fail_send_wr); //send work request 전송
+
+
+    printf("Success : Send request posted\n");
+
+    struct ibv_wc wc;//Work Complete
+    while(1){
+        int net = ibv_poll_cq(cq,1,&wc);
+        if(net<0){
+            printf("Fail to Poll CQ\n");
+            exit(1);
+        }
+        if(net=0){
+            continue;
+        }
+        if(wc.status!=IBV_WC_SUCCESS){
+            printf("Failed for wr_id %d\n",wc.wr_id);
+            exit(1);
+        }
+    }
+
+    ibv_destroy_qp(qp);
+    ibv_destroy_cq(comp_que);
+    ibv_dealloc_pd(pd);
+    ibv_close_device(context);
+    ibv_free_device_list(device_list);
+    free(msg_buf);
+
+    return 0;
+}
---
+++ rdma_server.c
@@ -0,0 +1,125 @@
+#include "rdma.h"
+
+int main(){
+    uint32_t remote_key;
+    char* msg;
+    int rc;
+
+    device_list=ibv_get_device_list(NULL);
+    if(!device_list){
+        printf("No IB device\n");
+        exit(1);
+    }
+    device=device_list[0];
+    if(!device){
+        printf("No IB device\n");
+        exit(1);
+    }
+    context=ibv_open_device_by_ibdev_name("mlx5_0");
+    if(!context){
+        printf("No Context device\n");
+        exit(1);
+    }
+    pd=ibv_alloc_pd(context);
+    if(!pd){
+        printf("No protection domain\n");
+        exit(1);
+    }
+    msg=(char*)malloc(MSG_SIZE);
+    mem_region=ibv_reg_mr(pd,msg,MSG_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE);
+
+    if(!mem_region){
+        printf("No mem region\n");
+        exit(1);
+    }
+//Complete Queue 생성
+    comp_que=ibv_create_cq(context,1,NULL,NULL,0);
+    if(!comp_que){
+        printf("No comp_que\n");
+        exit(1);
+    }
+
+    struct ibv_qp_init_attr qp_init_attr={};
+    memset(&qp_init_attr,0,sizeof(qp_init_attr));
+    qp_init_attr.send_cq=comp_que;
+    qp_init_attr.recv_cq=comp_que;
+    qp_init_attr.qp_type=IBV_QPT_RC; // queue pair type ; reliable connection
+    qp_init_attr.cap.max_send_wr=1; // Work Request
+    qp_init_attr.cap.max_recv_wr=10;
+    qp_init_attr.cap.max_send_sge=1; //Scatter gather entry
+    qp_init_attr.cap.max_recv_sge=1;
+
+
+//Queue pair 생성
+    qp=ibv_create_qp(pd,&qp_init_attr);
+    if(!qp){
+        printf("No queue pair\n");
+        exit(1);
+    }
+     // QP 상태 변경
+    struct ibv_qp_attr conn_attr={};
+    memset(&conn_attr,0,sizeof(conn_attr));
+    conn_attr.qp_state = IBV_QPS_INIT;
+    conn_attr.path_mtu=IBV_MTU_256;
+    conn_attr.pkey_index = 0;
+    conn_attr.port_num = 1;
+    conn_attr.qp_access_flags = IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE;
+    if (ibv_modify_qp(qp, &conn_attr, IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT | IBV_QP_ACCESS_FLAGS)) {
+        fprintf(stderr, "Failed to modify QP state to INIT: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    // QP 연결 대기
+    memset(&conn_attr, 0, sizeof(conn_attr));
+    conn_attr.qp_state = IBV_QPS_RTR;
+    conn_attr.path_mtu = IBV_MTU_256;
+    conn_attr.dest_qp_num = remote_qpn;
+    conn_attr.rq_psn = remote_psn;
+    conn_attr.max_dest_rd_atomic = 1;
+    conn_attr.min_rnr_timer = 12;
+    conn_attr.ah_attr.is_global = 0;
+    conn_attr.ah_attr.dlid = remote_lid;
+    conn_attr.ah_attr.sl = 0;
+    conn_attr.ah_attr.src_path_bits = 0;
+    conn_attr.ah_attr.port_num = 1;
+    if (ibv_modify_qp(qp, &conn_attr, IBV_QP_STATE | IBV_QP_AV | IBV_QP_PATH_MTU |
+                    IBV_QP_DEST_QPN | IBV_QP_RQ_PSN | IBV_QP_MAX_DEST_RD_ATOMIC |
+                    IBV_QP_MIN_RNR_TIMER)) {
+        fprintf(stderr, "Failed to modify QP state to RTR: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    //QP 연결 완료
+    memset(&conn_attr, 0, sizeof(conn_attr));
+    conn_attr.qp_state = IBV_QPS_RTS;
+    conn_attr.sq_psn = local_psn;
+    conn_attr.timeout = 14;
+    conn_attr.retry_cnt = 7;
+    conn_attr.rnr_retry = 7;
+    conn_attr.max_rd_atomic = 1;
+    if (ibv_modify_qp(qp, &conn_attr, IBV_QP_STATE | IBV_QP_SQ_PSN | IBV_QP_TIMEOUT |
+                    IBV_QP_RETRY_CNT | IBV_QP_RNR_RETRY | IBV_QP_MAX_QP_RD_ATOMIC)) {
+        fprintf(stderr, "Failed to modify QP state to RTS: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+        // 클라이언트가 보낸 문자열 받기
+    if (ibv_post_recv(qp, &r_wr, &bad_r_wr)) {
+        fprintf(stderr, "Failed to post receive: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // 버퍼에서 문자열 읽기
+    if (ibv_poll_cq(cq, 1, &wc) < 1) {
+        fprintf(stderr, "Failed to poll receive CQ: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (wc.status != IBV_WC_SUCCESS) {
+        fprintf(stderr, "Failed to receive message: %s\n", ibv_wc_status_str(wc.status));
+        exit(EXIT_FAILURE);
+    }
+
+    // 받은 문자열 출력하기
+    printf("Received message from client: %s\n", recv_buf);
+
+}
+
