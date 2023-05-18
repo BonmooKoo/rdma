2c73f0c0aa5674c6034191a61eff720a65275c11
BonmooKoo <rnqhsan9812@gmail.com>
2023-05-18 13:24:11 +0900

    Array

--- rdma_common.c
+++ rdma_common.c
@@ -148,48 +148,48 @@ int process_work_completion_events (struct ibv_comp_channel *comp_channel,
 	ret = ibv_get_cq_event(comp_channel, /* IO channel where we are expecting the notification */ 
 		       &cq_ptr, /* which CQ has an activity. This should be the same as CQ we created before */ 
 		       &context); /* Associated CQ user context, which we did set */
-       if (ret) {
-	       rdma_error("Failed to get next CQ event due to %d \n", -errno);
-	       return -errno;
-       }
+	if (ret) {
+		rdma_error("Failed to get next CQ event due to %d \n", -errno);
+		return -errno;
+	}
        /* Request for more notifications. */
-       ret = ibv_req_notify_cq(cq_ptr, 0);
-       if (ret){
-	       rdma_error("Failed to request further notifications %d \n", -errno);
-	       return -errno;
-       }
-       /* We got notification. We reap the work completion (WC) element. It is 
+	ret = ibv_req_notify_cq(cq_ptr, 0);
+	if (ret){
+		rdma_error("Failed to request further notifications %d \n", -errno);
+		return -errno;
+	}
+	/* We got notification. We reap the work completion (WC) element. It is 
 	* unlikely but a good practice it write the CQ polling code that 
        * can handle zero WCs. ibv_poll_cq can return zero. Same logic as 
        * MUTEX conditional variables in pthread programming.
 	*/
-       total_wc = 0;
-       do {
-	       ret = ibv_poll_cq(cq_ptr /* the CQ, we got notification for */, 
-		       max_wc - total_wc /* number of remaining WC elements*/,
-		       wc + total_wc/* where to store */);
-	       if (ret < 0) {
-		       rdma_error("Failed to poll cq for wc due to %d \n", ret);
-		       /* ret is errno here */
-		       return ret;
-	       }
-	       total_wc += ret;
-       } while (total_wc < max_wc); 
-       debug("%d WC are completed \n", total_wc);
+	total_wc = 0;
+	do {
+		ret = ibv_poll_cq(cq_ptr /* the CQ, we got notification for */, 
+			max_wc - total_wc /* number of remaining WC elements*/,
+			wc + total_wc/* where to store */);
+		if (ret < 0) {
+			rdma_error("Failed to poll cq for wc due to %d \n", ret);
+			/* ret is errno here */
+			return ret;
+		}
+		total_wc += ret;
+	} while (total_wc < max_wc); 
+	debug("%d WC are completed \n", total_wc);
        /* Now we check validity and status of I/O work completions */
-       for( i = 0 ; i < total_wc ; i++) {
-	       if (wc[i].status != IBV_WC_SUCCESS) {
-		       rdma_error("Work completion (WC) has error status: %s at index %d", 
-				       ibv_wc_status_str(wc[i].status), i);
-		       /* return negative value */
-		       return -(wc[i].status);
-	       }
-       }
-       /* Similar to connection management events, we need to acknowledge CQ events */
-       ibv_ack_cq_events(cq_ptr, 
-		       1 /* we received one event notification. This is not 
-		       number of WC elements */);
-       return total_wc; 
+	for( i = 0 ; i < total_wc ; i++) {
+		if (wc[i].status != IBV_WC_SUCCESS) {
+			rdma_error("Work completion (WC) has error status: %s at index %d", 
+					ibv_wc_status_str(wc[i].status), i);
+			/* return negative value */
+			return -(wc[i].status);
+		}
+	}
+	/* Similar to connection management events, we need to acknowledge CQ events */
+	ibv_ack_cq_events(cq_ptr, 
+			1 /* we received one event notification. This is not 
+			number of WC elements */);
+	return total_wc; 
 }
 
 
